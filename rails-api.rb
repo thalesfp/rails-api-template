37fe5a48feb107eae023dd33707a30bf1b1c45e3
Thales Pereira <thalesfp@gmail.com>
2016-03-13 03:13:28 -0300

    Separate config files into dirs

--- rails-api.rb
+++ rails-api.rb
@@ -1,3 +1,10 @@
+# Add the current directory to the path Thor uses
+# to look up files
+def source_paths
+  [File.expand_path(File.dirname(__FILE__))] + Array(super)
+    
+end
+
 Paperclip = yes?("Add Paperclip for file upload?")
 AWS = yes?("Add AWS SDK?")
 Knock = yes?("Add Knock for authentication?")
@@ -57,60 +64,6 @@ after_bundle do
   remove_dir "app/assets"
   remove_dir "test"
 
-  create_file 'config/initializers/inflections.rb' do <<-EOF
-ActiveSupport::Inflector.inflections do |inflect|
-  inflect.clear
-
-  inflect.plural(/$/,  's')
-  inflect.plural(/(s)$/i,  '\1')
-  inflect.plural(/^(paí)s$/i, '\1ses')
-  inflect.plural(/(z|r)$/i, '\1es')
-  inflect.plural(/al$/i,  'ais')
-  inflect.plural(/el$/i,  'eis')
-  inflect.plural(/ol$/i,  'ois')
-  inflect.plural(/ul$/i,  'uis')
-  inflect.plural(/([^aeou])il$/i,  '\1is')
-  inflect.plural(/m$/i,   'ns')
-  inflect.plural(/^(japon|escoc|ingl|dinamarqu|fregu|portugu)ês$/i,  '\1eses')
-  inflect.plural(/^(|g)ás$/i,  '\1ases')
-  inflect.plural(/ão$/i,  'ões')
-  inflect.plural(/^(irm|m)ão$/i,  '\1ãos')
-  inflect.plural(/^(alem|c|p)ão$/i,  '\1ães')
-
-  # Sem acentos...
-  inflect.plural(/ao$/i,  'oes')
-  inflect.plural(/^(irm|m)ao$/i,  '\1aos')
-  inflect.plural(/^(alem|c|p)ao$/i,  '\1aes')
-
-  inflect.singular(/([^ê])s$/i, '\1')
-  inflect.singular(/^(á|gá|paí)s$/i, '\1s')
-  inflect.singular(/(r|z)es$/i, '\1')
-  inflect.singular(/([^p])ais$/i, '\1al')
-  inflect.singular(/eis$/i, 'el')
-  inflect.singular(/ois$/i, 'ol')
-  inflect.singular(/uis$/i, 'ul')
-  inflect.singular(/(r|t|f|v)is$/i, '\1il')
-  inflect.singular(/ns$/i, 'm')
-  inflect.singular(/sses$/i, 'sse')
-  inflect.singular(/^(.*[^s]s)es$/i, '\1')
-  inflect.singular(/ães$/i, 'ão')
-  inflect.singular(/aes$/i, 'ao')
-  inflect.singular(/ãos$/i, 'ão')    
-  inflect.singular(/aos$/i, 'ao')
-  inflect.singular(/ões$/i, 'ão')
-  inflect.singular(/oes$/i, 'ao')
-  inflect.singular(/(japon|escoc|ingl|dinamarqu|fregu|portugu)eses$/i, '\1ês')
-  inflect.singular(/^(g|)ases$/i,  '\1ás')
-
-  # Incontáveis
-  inflect.uncountable %w( tórax tênis ônibus lápis fênix )
-
-  # Irregulares
-  inflect.irregular "país", "países"
-end
-EOF
-end
-
   insert_into_file 'app/controllers/application_controller.rb', after: "class ApplicationController < ActionController::API\n" do <<-EOF
     include ActionController::Serialization
   EOF
@@ -135,237 +88,22 @@ config/application.yml
 
     BR = yes?("Change i18n default locale to pt-BR?")
 
+    inside 'initializers' do
+      copy_file 'inflections.rb' if BR
+    end
+
+    inside 'locales' do
+      copy_file 'pt-BR.yml' if BR
+    end
+
     insert_into_file 'application.rb', after: "# config.i18n.default_locale = :de\n" do <<-EOF
     config.i18n.default_locale = 'pt-BR'
     config.i18n.fallbacks = [:en]
     EOF
     end if BR
-    
-    create_file 'locales/pt-BR.yml' do <<-EOF
----
-pt-BR:
-  activerecord:
-    attributes:
-      usuario:
-        password: "Senha"
-      oferta:
-        validade_inicial: "Validade Inicial"
-        validade_final: "Validade Final"
-    errors:
-      models:
-        oferta:
-          attributes:
-            validade_inicio:
-              invalid_datetime: "data inválida"
-              on_or_after: "precisa ser maior ou igual a hoje"
-            validade_final:
-              invalid_datetime: "data inválida"
-              after: "precisa ser maior que a validade inicial"
-  date:
-    abbr_day_names:
-    - Dom
-    - Seg
-    - Ter
-    - Qua
-    - Qui
-    - Sex
-    - Sáb
-    abbr_month_names:
-    - 
-    - Jan
-    - Fev
-    - Mar
-    - Abr
-    - Mai
-    - Jun
-    - Jul
-    - Ago
-    - Set
-    - Out
-    - Nov
-    - Dez
-    day_names:
-    - Domingo
-    - Segunda-feira
-    - Terça-feira
-    - Quarta-feira
-    - Quinta-feira
-    - Sexta-feira
-    - Sábado
-    formats:
-      default: "%d/%m/%Y"
-      long: "%d de %B de %Y"
-      short: "%d de %B"
-    month_names:
-    - 
-    - Janeiro
-    - Fevereiro
-    - Março
-    - Abril
-    - Maio
-    - Junho
-    - Julho
-    - Agosto
-    - Setembro
-    - Outubro
-    - Novembro
-    - Dezembro
-    order:
-    - :day
-    - :month
-    - :year
-  datetime:
-    distance_in_words:
-      about_x_hours:
-        one: aproximadamente 1 hora
-        other: aproximadamente %{count} horas
-      about_x_months:
-        one: aproximadamente 1 mês
-        other: aproximadamente %{count} meses
-      about_x_years:
-        one: aproximadamente 1 ano
-        other: aproximadamente %{count} anos
-      almost_x_years:
-        one: quase 1 ano
-        other: quase %{count} anos
-      half_a_minute: meio minuto
-      less_than_x_minutes:
-        one: menos de um minuto
-        other: menos de %{count} minutos
-      less_than_x_seconds:
-        one: menos de 1 segundo
-        other: menos de %{count} segundos
-      over_x_years:
-        one: mais de 1 ano
-        other: mais de %{count} anos
-      x_days:
-        one: 1 dia
-        other: "%{count} dias"
-      x_minutes:
-        one: 1 minuto
-        other: "%{count} minutos"
-      x_months:
-        one: 1 mês
-        other: "%{count} meses"
-      x_seconds:
-        one: 1 segundo
-        other: "%{count} segundos"
-    prompts:
-      day: Dia
-      hour: Hora
-      minute: Minuto
-      month: Mês
-      second: Segundo
-      year: Ano
-  errors:
-    format: "%{attribute} %{message}"
-    messages:
-      accepted: deve ser aceito
-      blank: não pode ficar em branco
-      present: deve ficar em branco
-      confirmation: não é igual a %{attribute}
-      empty: não pode ficar vazio
-      equal_to: deve ser igual a %{count}
-      even: deve ser par
-      exclusion: não está disponível
-      greater_than: deve ser maior que %{count}
-      greater_than_or_equal_to: deve ser maior ou igual a %{count}
-      inclusion: não está incluído na lista
-      invalid: não é válido
-      less_than: deve ser menor que %{count}
-      less_than_or_equal_to: deve ser menor ou igual a %{count}
-      not_a_number: não é um número
-      not_an_integer: não é um número inteiro
-      odd: deve ser ímpar
-      record_invalid: 'A validação falhou: %{errors}'
-      restrict_dependent_destroy:
-        one: Não é possível excluir o registro pois existe um %{record} dependente
-        many: Não é possível excluir o registro pois existem %{record} dependentes
-      taken: já está em uso
-      too_long: 'é muito longo (máximo: %{count} caracteres)'
-      too_short: 'é muito curto (mínimo: %{count} caracteres)'
-      wrong_length: não possui o tamanho esperado (%{count} caracteres)
-      other_than: deve ser diferente de %{count}
-    template:
-      body: 'Por favor, verifique o(s) seguinte(s) campo(s):'
-      header:
-        one: 'Não foi possível gravar %{model}: 1 erro'
-        other: 'Não foi possível gravar %{model}: %{count} erros.'
-  helpers:
-    select:
-      prompt: Por favor selecione
-    submit:
-      create: Criar %{model}
-      submit: Salvar %{model}
-      update: Atualizar %{model}
-  number:
-    currency:
-      format:
-        delimiter: "."
-        format: "%u %n"
-        precision: 2
-        separator: ","
-        significant: false
-        strip_insignificant_zeros: false
-        unit: R$
-    format:
-      delimiter: "."
-      precision: 3
-      separator: ","
-      significant: false
-      strip_insignificant_zeros: false
-    human:
-      decimal_units:
-        format: "%n %u"
-        units:
-          billion:
-            one: bilhão
-            other: bilhões
-          million:
-            one: milhão
-            other: milhões
-          quadrillion:
-            one: quatrilhão
-            other: quatrilhões
-          thousand: mil
-          trillion:
-            one: trilhão
-            other: trilhões
-          unit: ''
-      format:
-        delimiter: "."
-        precision: 2
-        significant: true
-        strip_insignificant_zeros: true
-      storage_units:
-        format: "%n %u"
-        units:
-          byte:
-            one: Byte
-            other: Bytes
-          gb: GB
-          kb: KB
-          mb: MB
-          tb: TB
-    percentage:
-      format:
-        delimiter: "."
-        format: "%n%"
-    precision:
-      format:
-        delimiter: "."
-  support:
-    array:
-      last_word_connector: " e "
-      two_words_connector: " e "
-      words_connector: ", "
-  time:
-    am: ''
-    formats:
-      default: "%a, %d de %B de %Y, %H:%M:%S %z"
-      long: "%d de %B de %Y, %H:%M"
-      short: "%d de %B, %H:%M"
-    pm: ''
+
+    insert_into_file 'application.rb', after: "# config.time_zone = 'Central Time (US & Canada)'\n" do <<-EOF
+    config.time_zone = 'Brasilia'
     EOF
     end if BR
 
@@ -441,12 +179,20 @@ test:
     gsub_file 'routes.rb', /^\s{0,}#.*\n{0,1}$/, ''
     gsub_file 'routes.rb', /^\n$/, ""
 
+    insert_into_file 'routes.rb', after: "Rails.application.routes.draw do\n" do <<-EOF
+  
+  api_version(module: "V1", path: {value: "v1"}) do
+
+  end
+    EOF
+    end
+
     gsub_file 'database.yml', /^\s{0,}#.*\n{0,1}$/, ''
     gsub_file 'database.yml', /\n{1,}$/, "\n"
 
     gsub_file 'database.yml', /database: #{@app_name}_(development|test|production)/, "database: <%= ENV['database_name'] %>"
     gsub_file 'database.yml', "username: #{@app_name}", "username: <%= ENV['database_username'] %>"
-    gsub_file 'database.yml', "password: <%= ENV[\'#{@app_name.upcase}_DATABASE_PASSWORD\'] %>", "password: <%= ENV['database_password'] %>"
+    gsub_file 'database.yml', "password: <%= ENV['#{@app_name.upcase}_DATABASE_PASSWORD'] %>", "password: <%= ENV['database_password'] %>"
   end
 
   run "spring stop"
